89e2497231d64218c6f30cfd322d26025a5eb75d
bradley-spencer <bspencer@greenless.org>
2024-04-03 20:42:53 -0700

    first assignment

---
+++ 4-8-24/receipt.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+name = input("Enter your name: ")
+amount = input("Enter the amount of your purchase: ")
+
+amount = float(amount)
+tax = amount * 0.10
+total = amount + tax
+
+print("Hello", name, "here is your sales receipt:")
+print("Subtotal = $", format(amount, ',.2f').rjust(8, ' '))
+print("     Tax = $", format(tax, ',.2f').rjust(8, ' '))
+print("             --------")
+print("   Total = $", format(total, ',.2f').rjust(8, ' '))
